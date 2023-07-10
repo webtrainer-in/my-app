6a2ffd7b0d712b5ae7e755df148f989efdcf3fea
Sandeep <webtrainer.in@gmail.com>
2023-07-10 11:39:04 +0530

    Make changes in API and Angular
    Signed-off-by: Sandeep <webtrainer.in@gmail.com>

--- API/API/Controllers/WeatherForecastController.cs
+++ API/API/Controllers/WeatherForecastController.cs
@@ -31,7 +31,7 @@ namespace API.Controllers
       {
         Date = DateTime.Now.AddDays(index),
         TemperatureC = rng.Next(-20, 55),
-        Summary = Summaries[rng.Next(Summaries.Length)]
+        Summary2 = Summaries[rng.Next(Summaries.Length)]
       })
       .ToArray();
     }
